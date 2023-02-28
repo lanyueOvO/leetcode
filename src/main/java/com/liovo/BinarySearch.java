1ef58be021074f2691722e97e12887bf17d54501
liovo <1801998377@qq.com>
2023-02-28 23:23:29 +0800

    333

--- src/main/java/com/liovo/BinarySearch.java
+++ src/main/java/com/liovo/BinarySearch.java
@@ -7,11 +7,10 @@ public class BinarySearch {
      */
     public static int binarySearchBasic(int[] nums,int target){
         if (nums != null && nums.length>0) {
-            for (int i=0;i<nums.length-1;i++){
-                for (int j = nums.length-1; j <nums.length ; j++) {
-                    if (i>j) return -1; //如果没查到直接返回-1
-                    int m = (int) Math.floor((i+j)/2); //向下取整
-                    System.out.println(m);
+            for (int i=0;i<nums.length-1;i--){
+                for (int j = nums.length-1; j <nums.length+1 ; j++) {
+                    if (i>=j) return -1; //如果没查到直接返回-1
+                    int m = (i+j)>>1; //向下取整
                     if (nums[m]>target){
                         j=m-1;
                     }else if (nums[m]<target){
@@ -26,14 +25,15 @@ public class BinarySearch {
         return -1;
     }
 
-    public static int binarySearchBasic2(int nums[],int target){
+//    改进版1
+    public static int binarySearchBasic2(int[] nums,int target){
         int i = 0 , j= nums.length-1; //设置指针和初始值
         while (i<=j){
-            int m = (i+j)/2;
+            int m = (i+j)>>1;
             if (target<nums[m]){  //若目标在左边
-                j = m -1;
+                j = m - 1;
             }else if (target> nums[m]){
-                i = m+1;
+                i = m + 1;
             }else {
                 return m;
             }
@@ -41,8 +41,30 @@ public class BinarySearch {
         return -1;
     }
 
-    public static void main(String[] args) {
-        int[] nums = {7,13,21,30,38,44,52,54};
-        System.out.println("now:"+binarySearchBasic2(nums,52));
+//    改进版2
+    public static int binarySearchBasic3(int[] nums,int target){
+        int i = 0 , j = nums.length;
+        while (1<j-i){
+            int m = (j+i)>>1;
+            if (target<nums[i]){
+                j=m;
+            }else {
+                i=m;
+            }
+        }
+        if (nums[i] == target){
+            return i;
+        }else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        long startTime = System.nanoTime();
+        int[] nums = {7,13,21,30,38,44,52,54,90,91,92,93,94,95,96,97,98};
+//        Thread.sleep(100);
+        System.out.println("now:"+binarySearchBasic(nums,91));
+        long endTime = System.nanoTime();
+        System.out.println(endTime-startTime);
     }
 }
